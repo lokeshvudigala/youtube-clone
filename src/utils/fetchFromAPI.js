7ed954efd8fb7114efb1d1510ef42d95e75a6305
lokeshmanikanta <lokeshmanikantha@gmail.com>
2024-09-16 15:48:44 +0530

    final changes

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "x-rapidapi-key": "ed3846b59cmsh9276ea3649e6f29p1c27efjsnf730f677e118",
+    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
